0d0729794424e59b8394df8ed904d6a2ebb22b99
Gid <mkazli733@gmail.com>
2019-11-27 16:24:52 +0200

    added crud schematic

---
+++ src/bb-crud/index.ts
@@ -0,0 +1,34 @@
+import { strings } from '@angular-devkit/core';
+import { apply, filter, mergeWith, move, noop, Rule, template, url } from '@angular-devkit/schematics';
+import { uppercase } from "../utility/uppercase-helper";
+
+import { parseName } from '../utility/parse-name';
+import { validateName } from '../utility/validation';
+import { Schema as FnComponentOptions } from './schema';
+
+export function bbCrud(options: FnComponentOptions): Rule {
+  if (options.path === undefined) {
+    options.path = '/src/';
+  }
+
+  const parsedPath = parseName(options.path, options.name);
+  options.name = parsedPath.name;
+  options.path = parsedPath.path;
+
+  validateName(options.name);
+
+  const templateSource = apply(url('./files'), [
+    options.noSpec ? filter(path => !path.endsWith('.test.__jsext__')) : noop(),
+    template({
+      ...strings,
+      uppercase,
+      'if-flat': (s: string) => options.subfolder ? s : '',
+      jsext: !!options.ts ? 'ts' : 'js',
+      jsxext: !!options.ts ? 'tsx' : 'js',
+      ...options,
+    }),
+    move(parsedPath.path),
+  ]);
+
+  return mergeWith(templateSource);
+}
